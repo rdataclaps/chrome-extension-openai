e519950ee2d3ff1876ae36a21d1929517c8b13c7
saurabh-anthwal <saurabhanthwal@gmail.com>
2024-02-16 15:23:27 +0530

    testing done

--- src/Components/Layout/Topbar.js
+++ src/Components/Layout/Topbar.js
@@ -1,14 +1,14 @@
-import React, { useState,useEffect } from "react";
-import user from "../assets/Images/Avatar/new_user.png";
-import Logo from "../assets/Icons/Logo.svg";
+import React, {useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import {Button} from "antd"
-import { downloadPdf, googleLogin } from "../../redux/actions/authActions";
+import {getUser, googleLogin } from "../../redux/actions/authActions";
 import { openEmailBox } from "../../redux/actions/userActions";
 
 function Topbar() {
   const dispatch = useDispatch()
-  const attributes = useSelector((state) => state.user.user);
+  const attributes = useSelector((state) => state?.user?.user);
+  const download_flag = useSelector((state)=>state?.user?.userData?.download_flag)
+  
   const handleGoogleApi =()=>{
     dispatch(googleLogin())
   }
@@ -17,29 +17,14 @@ function Topbar() {
     dispatch(openEmailBox(true))
   }
 
-
-  const [previousUrl, setPreviousUrl] = useState('');
-
   useEffect(() => {
-    const handleNavigation = () => {
-      setPreviousUrl(window.location.href);
-    };
-
-    // Listen for the beforeunload event to detect page navigation
-    window.addEventListener('beforeunload', handleNavigation);
-
-    // Clean up the event listener
-    return () => {
-      window.removeEventListener('beforeunload', handleNavigation);
-    };
-  }, []);
-console.log("proute",previousUrl)
-console.log("croute",window.location.href)
+    dispatch(getUser())
+  }, [dispatch]);
   return (
     <div className="topbar_nav_item">
       <div className="logo" style={{display:"flex",justifyContent:"space-between"}}>
       {/* <h1>askmail</h1> */}
-      {previousUrl !== window.location.href && (
+      {download_flag && (
       <div>
         <Button className="connect-with-gmail" onClick={handleDownloadPDF} type="primary" ghost>Download PDF</Button>
         </div>
